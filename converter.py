ee3e3d056df9246ee31d2055d42e858196ac0be3
Aaron Polhamus <aaronpolhamus@gmail.com>
2020-04-10 08:30:18 -0500

    updated offset

--- converter.py
+++ converter.py
@@ -6,7 +6,7 @@ parser = ArgumentParser(
     description="Convert a JPG of a black sketch against a contrasting background into a transparent PNG")
 
 parser.add_argument("--in_file", default="./marciana.jpg")
-parser.add_argument("--offset", type=int, default=-60)
+parser.add_argument("--offset", type=int, default=60)
 parser.add_argument("--steepness", type=float, default=0.25)
 parser.add_argument("--out_file", type=str, default="./marciana.png")
 args = parser.parse_args()
@@ -15,7 +15,7 @@ args = parser.parse_args()
 def sigmoid(value, offset=args.offset, steepness=args.steepness):
     """Set the transparency of a pixel based on how dark it is. Pixels that aren't pretty close to black should wash out
     """
-    return 255 / (1 + np.exp(steepness * (value + offset)))
+    return 255 / (1 + np.exp(steepness * (value - offset)))
 
 
 def make_rgba(image):
