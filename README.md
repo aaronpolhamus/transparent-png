9ca478eb2215ca1bd8b85b1f1ebfc04859436871
Aaron Polhamus <aaronpolhamus@gmail.com>
2020-04-10 15:40:47 -0500

    updated for multiple colors w/ euclidean distance

--- README.md
+++ README.md
@@ -21,6 +21,6 @@ You can point the converter at any file and manually specify the output file: `p
 ## Tuning the conversion
 I use a sigmoid function the looks at how dark a pixel is and, based on that, assigns it an alpha transparency. The sigmoid function is nice because it renders a smooth gradient against a transparent background, avoiding the awkward discontinuities that a simple threshold would create. The defaults I picked worked pretty well, but if you'd like to play with them this is the function you'll be adjusting:
 
-![Imgur](https://i.imgur.com/epNzyaz.png)
+![Imgur](https://i.imgur.com/epNzyaz.png)````
 
 `--offset` shifts the curve right and left, and `--steepness` controls the steepness of the transparency gradient. The larger this number, the closer your approximation to a strict threshold for transparency will be. 
--- converter.py
+++ converter.py
@@ -1,11 +1,19 @@
+"""This script file can place a transparent filter over an image based on an arbitrary number of colors. To use this
+functionality in triplets of RGB codes with simple space separation. To select red, green, and blue, for example, you
+would pass --targets 255 0 0 0 255 0 0 0 255. By default it the converter selects only the color black for filtering.
+"""
+
+from argparse import ArgumentParser
+from itertools import zip_longest
+
 import numpy as np
 from PIL import Image
-from argparse import ArgumentParser
 
 parser = ArgumentParser(
     description="Convert a JPG of a black sketch against a contrasting background into a transparent PNG")
 
 parser.add_argument("--in_file", default="./marciana.jpg")
+parser.add_argument("--targets", nargs="+", type=int, default=[0, 0, 0])
 parser.add_argument("--offset", type=int, default=60)
 parser.add_argument("--steepness", type=float, default=0.25)
 parser.add_argument("--out_file", type=str, default="./marciana.png")
@@ -15,19 +23,30 @@ args = parser.parse_args()
 def sigmoid(value, offset=args.offset, steepness=args.steepness):
     """Set the transparency of a pixel based on how dark it is. Pixels that aren't pretty close to black should wash out
     """
-    return 255 / (1 + np.exp(steepness * (value - offset)))
+    return 255/(1 + np.exp(steepness * (value - offset)))
 
 
-def make_rgba(image):
-    img = np.asarray(Image.open(image))
-    signature = np.mean(img, axis=2)
-    alpha = sigmoid(signature)
-    filtered_img = np.dstack((img, alpha))
-    img_with_alpha = Image.fromarray(filtered_img.astype('uint8'))
-    return img_with_alpha
+def grouper(n, iterable):
+    """https://stackoverflow.com/questions/1624883/alternative-way-to-split-a-list-into-groups-of-n"""
+    rgbs = [iter(iterable)] * n
+    return zip_longest(*rgbs)
 
 
 if __name__ == "__main__":
-    out_img = make_rgba(args.in_file)
-    out_path = args.out_file
-    out_img.save(out_path)
+    if not len(args.targets) % 3 == 0:
+        raise ValueError("--targets must receive RGB triplets of 3 numbers each. Use 0 to indicate a missing color")
+    targets = [item for item in grouper(3, args.targets)]
+    img = np.asarray(Image.open(args.in_file))
+    w, h, _ = img.shape
+    alphas = np.empty((w, h, len(targets)), dtype=int)
+    for i, target in enumerate(targets):
+        print(f"Generating mask for RGB target {target}")
+        target_array = np.full(img.shape, target, dtype=int)
+        diffs_array = img - target_array
+        dist_array = np.sqrt(np.sum(diffs_array**2, axis=2))
+        alphas[:, :, i] = sigmoid(dist_array)
+
+    alpha_mask = np.max(alphas, axis=2)
+    filtered_img = np.dstack((img, alpha_mask))
+    out_img = Image.fromarray(filtered_img.astype('uint8'))
+    out_img.save(args.out_file)
